20df9713b2b90ff2b1c5aa787c6162e8f99cb451
Tommy DevBox <tommytang213@gmail.com>
2026-09-07 17:05:49 +0000

    Remove unused group search test import

--- apps/mobile/test/ui/group_list_shared_search_test.dart
+++ apps/mobile/test/ui/group_list_shared_search_test.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/groups/group_list_screen.dart';
-import 'package:mobile/groups/group_repository.dart';
 import 'package:mobile/ui/settleora_components.dart';
 import 'package:mobile/ui/settleora_theme.dart';
 
